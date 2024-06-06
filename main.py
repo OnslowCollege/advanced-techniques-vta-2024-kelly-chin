54d275266af1945c0dcb02814a4305ff807bb742
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-06-06 00:05:47 +0000

    Jun 6, 2024, 12:05 AM

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ print("""Trivia Quiz Game
     Please enter the number of the choices below: 
     1. Play game
     2. Visit shop
-    3. How to play instructions""")
+    3. How to play instructions
+    4. Exit game""")
 
 user_choice: str = input("Enter the number of your choice: ")
+
