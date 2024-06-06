cfb1a828f381fa6cd4e37ee184cbe64d77f355a9
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-06-06 00:10:21 +0000

    Jun 6, 2024, 12:10 AM

--- main.py
+++ main.py
@@ -7,10 +7,27 @@ Date: 6/06/2024
 
 print("""Trivia Quiz Game
     Please enter the number of the choices below: 
-    1. Play game
+    1. Play game 
     2. Visit shop
     3. How to play instructions
     4. Exit game""")
 
 user_choice: str = input("Enter the number of your choice: ")
+
+program_running = True
+
+if user_choice == 1:
+    pass
+
+elif user_choice == 2:
+    pass
+
+elif user_choice == 3:
+    pass
+
+# Game program ends. 
+elif user_choice == 4:
+    print("Thanks for playing! Hope you have enjoyed it! ")
+    
+    program_running = False
 
