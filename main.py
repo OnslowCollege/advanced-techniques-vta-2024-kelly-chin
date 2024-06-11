a28eeb6d770e7f950f9677c38de4912a9cf3768b
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-06-11 01:19:30 +0000

    Jun 11, 2024, 1:19 AM

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ Trivia Quiz game || Project Management.
 Created by: Kelly
 Date: 6/06/2024
 """
+# Function prints shop items
+def shop() -> str:
+    """Print a shop."""
+    print("== XXX FOR SALE! ==")
+    for item in SHOP_ITEMS:
+        print(item)
+    return("")
+
+# How to play instructions defined
+def how_to_play() -> str:
+    """How to play instructions."""
+
+    return ("""
+    How to Play instructions: ❓ 
+
+    » Objective of game: Collect as many diamonds 💎 as you can. «
+
+    Depending on the level difficulty you choose from easy, medium, and hard, 
+    you will earn a random amount of diamonds per correct question.
+    
+    Every answer you answer correct, If answering the next question correctly, 
+    you will receive an uncertain reward, getting this question wrong, you
+    will not receive your reward AND lose diamonds. Diamonds can be spent for
+    goodies at the shop.
+
+    Good Luck and have fun! 🤖""")
 
 print("""
     Trivia Quiz Game
@@ -24,33 +50,6 @@ SHOP_ITEMS: dict[str, int] = {
 user_choice: int = int(input("\nEnter the number of your choice: "))
 
 program_running = True
-
-def shop() -> str:
-    """Print a shop."""
-    print("== XXX FOR SALE! ==")
-    for item in SHOP_ITEMS:
-        print(item)
-    return("")
-
-
-# How to play instructions defined
-def how_to_play() -> str:
-    """How to play instructions."""
-
-    return ("""
-    How to Play instructions: ❓ 
-
-    » Objective of game: Collect as many diamonds 💎 as you can. «
-
-    Depending on the level difficulty you choose from easy, medium, and hard, 
-    you will earn a random amount of diamonds per correct question.
-    
-    Every answer you answer correct, If answering the next question correctly, 
-    you will receive an uncertain reward, getting this question wrong, you
-    will not receive your reward AND lose diamonds. Diamonds can be spent for
-    goodies at the shop.
-
-    Good Luck and have fun! 🤖""")
 
 
 if user_choice == 1:
