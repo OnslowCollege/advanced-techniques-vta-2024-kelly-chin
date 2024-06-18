59b1b8086deef2e225168106a581202d73175050
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-06-18 01:34:50 +0000

    Jun 18, 2024, 1:34 AM

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ user_choice: int = int(input("\nEnter the number of your choice: "))
 valid_option: bool = True
 
 while valid_option: 
-    if valid_option is not
+    if valid_option is not == "1", "2", "3", or "4":
+    
     if user_choice == 1:
         print(difficulty_page())
 
