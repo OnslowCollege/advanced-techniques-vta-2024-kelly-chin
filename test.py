58a8ff090e745d091aaf353e6ae840eda882c4cc
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-07-02 23:05:58 +0000

    Jul 2, 2024, 11:05 PM

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 """test."""
-try:
-    # This code can raise an Exception.
-    age = int(input("Enter your age: "))
-except ValueError:
-    # If the ValueError exception is
-    # raised, this block of code will run
-    # instead of a crash happening.
-    print("Invalid age provided.")
+age: int = -1
+
+while age == -1:
+    try:
+        # This code can raise an Exception.
+        age = int(input("Enter your age: "))
+    except ValueError:
+        # If the ValueError exception is
+        # raised, this block of code will run
+        # instead of a crash happening.
+        print("Invalid age provided.")
