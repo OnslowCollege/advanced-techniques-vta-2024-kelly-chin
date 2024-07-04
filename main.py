1e6069c0fa756f7f07553641f9210f234a700a85
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-07-04 00:00:59 +0000

    Jul 4, 2024, 12:00 AM

--- main.py
+++ main.py
@@ -68,7 +68,8 @@ SHOP_ITEMS: dict[str, dict[str, int | int]] = {
 
 TRIVIA_QUESTIONS: dict[str, dict[str, str]] = {
     "Easy": {
-            "What country is the biggest in the world?": "Russia"},
+            "What country is the biggest in the world?": "Russia"
+            ""},
 
 
 }
