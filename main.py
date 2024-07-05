00f9f08862afe90490a73a5ca94aa2d534271a5a
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-07-05 03:04:24 +0000

    Jul 5, 2024, 3:04 AM

--- main.py
+++ main.py
@@ -77,8 +77,7 @@ TRIVIA_QUESTIONS: dict[str, dict[str, str]] = {
         "What element do humans need to survive?": "Oxygen",
         'What does the fast food chain, "KFC" stand for?': "Kentucky Fried Chicken",
         "What is the name of the Greek Goddess of Love and Beauty?": "Aphrodite",
-
-
+        "What animal is the fastest in the world?": "Cheetah",
     },
     "Medium": {
         "What country is the only country with a triangular flag?": "Nepal",
@@ -88,7 +87,7 @@ TRIVIA_QUESTIONS: dict[str, dict[str, str]] = {
         'What year did the "y2k" problem occur?': "2000",
         "What language did the ancient Romans speak?": "Latin",
         "Which volcano in Indonesia caused the loudest sound in history?": "Krakatoa",
-        
+        "What organ is the largest in the human body?": "Skin",
     },
     "Hard": {
         'In the film "Dead Poets Society", which actor played "Neil Perry?"': "Robert Sean Leonard",
@@ -97,6 +96,7 @@ TRIVIA_QUESTIONS: dict[str, dict[str, str]] = {
         "How many hearts does an octopus have?": "3",
         "Which country is the only one that has the bible on their flag?": "Dominician Republic",
         'Which greek philosopher famously said, "Man is the measure of all things?': "Protogoras",
+        "What shark species was the largest to have ever lived?": "Megalodon",
     },
 }
 
