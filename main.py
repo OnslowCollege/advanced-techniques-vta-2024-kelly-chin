5c9507a24c88abc2356649cfcadf66fc4f47774e
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-07-24 23:59:38 +0000

    Jul 24, 2024, 11:59 PM

--- main.py
+++ main.py
@@ -58,6 +58,8 @@ def difficulty_page() -> str:
                     » Hard 😡(H) «
         
 """)
+# Bool dictates whether if the user input is valid  
+valid_option: bool = False
 
 difficulty_choice = str(input("Enter your choice of level difficulty: "))
 
@@ -86,8 +88,6 @@ def question() -> str:
 
 # bool dictates whether if program is running.
 program_running: bool = True
-# Bool dictates whether if the user input is valid  
-valid_option: bool = False
 
 # Stores shop items from a dictionary.
 SHOP_ITEMS: dict[str, dict[str, int | int]] = {
