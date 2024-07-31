0b1b75d7be2a0af97537a534b6e6b2d775ddfd65
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-07-31 23:24:39 +0000

    Jul 31, 2024, 11:24 PM

--- main.py
+++ main.py
@@ -60,42 +60,45 @@ SHOP_ITEMS: dict[str, dict[str, int | int]] = {
     "item 4": {"Price": 1000, "Quantity Owned": 0},
 }
 
-# Dictionary of trivia questions, including easy, medium and hard difficulty.
-TRIVIA_QUESTIONS: dict[str, dict[str, str]] = {
-    "Easy": {
-        "What country is the largest in the world?": "Russia",
-        "Which sport uses the terms, Spare and Strike?": "Bowling",
-        "What is the 4th letter in the English alphabet?": "D",
-        "What country did french fries originate?": "Belgium",
-        "Which galaxy do we live in?": "The Milky Way",
-        "What element do humans need to survive?": "Oxygen",
-        'What does the fast food chain, "KFC" stand for?': "Kentucky Fried Chicken",
-        "What is the name of the Greek Goddess of Love and Beauty?": "Aphrodite",
-        "What animal is the fastest in the world?": "Cheetah",
-        "Who discovered gravity?": "Isaac Newton",
-    },
-    "Medium": {
-        "What country is the only country with a triangular flag?": "Nepal",
-        "What is the 6th element in the periodic table?": "Nitrogen",
-        'Which planet is known as the "Blue Planet"?': "Earth",
-        "What do you call baby kangaroos?": "Joey",
-        'What year did the "y2k" problem occur?': "2000",
-        "What language did the ancient Romans speak?": "Latin",
-        "Which volcano in Indonesia caused the loudest sound in history?": "Krakatoa",
-        "What organ is the largest in the human body?": "Skin",
-    },
-    "Hard": {
-        'In the film "Dead Poets Society", which actor played "Neil Perry?"': "Robert Sean Leonard",
-        "What is the only parrot that cannot fly?": "Kakapo",
-        "Which Asian country fought in 7 deadliests wars in history?": "China",
-        "How many hearts does an octopus have?": "3",
-        "Which country is the only one that has the bible on their flag?": "Dominician Republic",
-        'Which greek philosopher famously said, "Man is the measure of all things?': "Protogoras",
-        "What shark species was the largest to have ever lived?": "Megalodon",
-        'What character did Eliza Taylor play in the TV series, "The 100"?': "Clarke Griffins",
-    },
-}
+# 2d list for trivia questions.
 
+easy_questions: list[list[str]] = [
+[""]
+
+
+
+]
+
+
+medium_questions: list[list[str]] = [
+
+
+
+]
+
+hard_questions: list[list[str]] = [
+
+
+
+]
+
+easy_answers: list[list[str]] = [
+
+
+
+]
+
+medium_answers: list[list[str]] = [
+
+
+
+]
+
+hard_answers: list[list[str]] = [
+
+
+
+]
 # Function that asks users easy trivia questions.
 def ask_easy_questions() -> str:
     """Asks user easy questions."""
