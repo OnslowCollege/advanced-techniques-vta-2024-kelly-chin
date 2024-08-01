fb57e57b0d8b95491c853e60dbeb499039e0fc56
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-08-01 07:50:43 +0000

    Aug 1, 2024, 7:50 AM

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ while valid_option is False:
         user_choice = int(input("\nEnter the number of your choice: "))
         if user_choice == 1:
             print(difficulty_page())
-            difficulty_choice = str(input("Enter your choice of level difficulty: "))
+            difficulty_choice = str(input("Enter level difficulty: "))
             if difficulty_choice.upper() == "E":
                 # Prints easy questions.
                 for question in TRIVIA_QUESTIONS["Easy"]:
