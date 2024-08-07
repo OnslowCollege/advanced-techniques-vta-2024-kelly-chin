8b89f379ecf228a0a5fd470cf0ab2358d2234d2e
oc-kelly-chin-2024 <159388682+oc-kelly-chin-2024@users.noreply.github.com>
2024-08-07 23:57:18 +0000

    Aug 7, 2024, 11:57 PM

--- main.py
+++ main.py
@@ -69,85 +69,104 @@ SHOP_ITEMS: dict[str, dict[str, int | int]] = {
 # Dictionary of trivia questions, including easy, medium and hard difficulty.
 TRIVIA_QUESTIONS = {
     "Easy": {
-        {"question": "What country is the largest in the world?",
-        "answer": "Russia"},
-        {"question": "Which sport uses the terms, Spare and Strike?",
-        "answer": "Bowling"},
-        {"question": "What is the 4th letter in the English alphabet?",
-        "answer": "D"},
-        {"question": "What country did french fries originate?",
-        "answer": "Belgium"},
+        {
+            "question": "What country is the largest in the world?",
+            "answer": "Russia"
+        },
+        {
+            "question": "Which sport uses the terms, Spare and Strike?",
+            "answer": "Bowling"
+        },
+        {
+            "question": "What is the 4th letter in the English alphabet?",
+            "answer": "D"
+        },
+        {
+            "question": "What country did french fries originate?",
+            "answer": "Belgium"
+        },
         {"question": "Which galaxy do we live in?", "answer": "The Milky Way"},
-        {"question": "What element do humans need to survive?",
-        "answer": "Oxygen"},
-        {"question": 'What does fast food chain, "KFC" stand for?',
-        "answer": "Kentucky Fried Chicken"},
-        {"question": "What is the name of the Greek Goddess of Love and Beauty?",
-        "answer": "Aphrodite"},
-        {"question": "What animal is the fastest in the world?",
-        "answer": "Cheetah"},
+        {
+            "question": "What element do humans need to survive?",
+            "answer": "Oxygen"
+        },
+        {
+            "question": 'What does fast food chain, "KFC" stand for?',
+            "answer": "Kentucky Fried Chicken"
+        },
+        {
+            "question": "What is the name of the Greek Goddess of Love and Beauty?",
+            "answer": "Aphrodite"
+        },
+        {
+            "question": "What animal is the fastest in the world?",
+            "answer": "Cheetah"
+        },
         {"question": "Who discovered gravity?", "answer": "Isaac Newton"},
-            },
+    },
     "Medium": {
-        1:{
+        1: {
             "question": "What country is the only country with a triangular flag?",
-            "answer": "Nepal",
+            "answer": "Nepal"
         },
-        2:{
+        2: {
             "question": "What is the 6th element in the periodic table?",
-            "answer": "Nitrogen",
+            "answer": "Nitrogen"
         },
-        3:{
+        3: {
             "question": 'Which planet is known as the "Blue Planet"?',
-            "answer": "Earth",
+            "answer": "Earth"
         },
-        4:{"question": "What do you call baby kangaroos?", "answer": "Joey"},
-        5:{
+        4: {"question": "What do you call baby kangaroos?", "answer": "Joey"},
+        5: {
             "question": 'What year did the "y2k" problem occur?',
-            "answer": "2000",
+            "answer": "2000"
         },
-        6:{
+        6: {
             "question": "What language did the ancient Romans speak?",
-            "answer": "Latin",
+            "answer": "Latin"
         },
-        7:{
+        7: {
             "question": "Which volcano in Indonesia caused the loudest sound in history?",
-            "answer": "Krakatoa",
+            "answer": "Krakatoa"
         },
-        8:{
+        8: {
             "question": "What organ is the largest in the human body?",
-            "answer": "Skin",
+            "answer": "Skin"
         },
     },
     "Hard": {
-        1:{
+        1: {
             "question": 'In the film "Dead Poets Society"(1989), who played "Neil Perry?"',
-            "answer": "Robert Sean Leonard",
+            "answer": "Robert Sean Leonard"
         },
-        2:{
+        2: {
             "question": "What is the only parrot that cannot fly?",
-            "answer": "Kakapo",
+            "answer": "Kakapo"
         },
-        3:{
+        3: {
             "question": "Which Asian country fought in 7 deadliests wars in history?",
-            "answer": "China",
+            "answer": "China"
         },
-        4:{"question": "How many hearts does an octopus have?", "answer": "3"},
-        5:{
+        4: {
+            "question": "How many hearts does an octopus have?",
+            "answer": "3"
+        },
+        5: {
             "question": "Which country is the only one that has the bible on their flag?",
-            "answer": "Dominician Republic",
+            "answer": "Dominician Republic"
         },
-        6:{
+        6: {
             "question": 'Which greek philosopher famously said, "Man is the measure of all things?',
-            "answer": "Protogoras",
+            "answer": "Protogoras"
         },
-        7:{
+        7: {
             "question": "What shark species was the largest to have ever lived?",
-            "answer": "Megalodon",
+            "answer": "Megalodon"
         },
-        8:{
+        8: {
             "question": 'What character did Eliza Taylor play in the TV series, "The 100"?',
-            "answer": "Clarke Griffins",
+            "answer": "Clarke Griffins"
         },
     },
 }
